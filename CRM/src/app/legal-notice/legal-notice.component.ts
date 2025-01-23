460761cb5698a9951dcbc62d155b3c21e57cba76
Hasan Korkmaz <hasan_k2023@outlook.com>
2025-01-23 10:10:02 +0100

    dashboard responisive

--- CRM/src/app/legal-notice/legal-notice.component.ts
+++ CRM/src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  constructor( private location: Location,  private route: ActivatedRoute,) {}
 
+
+  goBack() {
+    this.location.back();
+  }
 }
